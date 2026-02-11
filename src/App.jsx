298eac980006a169f28abfbeb8e256816e925360
blockchaingurukerala <blockchaingurukerala@gmail.com>
2026-02-11 10:58:00 +0000

    registration done

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import './App.css'
 import abi from './contractabi.json'
 import { BrowserProvider, Contract } from 'ethers'
 function App() {
+  const CONTRACT_ADDRESS="0x9051807EFD07f0dD3941d8B5C0F684f4A5E9753E"
   const [count, setCount] = useState(0)
   const registerStudent= async () => {
-    console.log("clickede Student")
+    const provider=new BrowserProvider(window.ethereum);
+    const signer= await provider.getSigner();
+    const contract= new Contract(CONTRACT_ADDRESS,abi,signer);
+    await contract.register(2);
+    alert("Successfully registered as Student")
   }
   const registerTeacher= async () => {
-    console.log("clickede Teacher")
+    const provider=new BrowserProvider(window.ethereum);
+    const signer= await provider.getSigner();
+    const contract= new Contract(CONTRACT_ADDRESS,abi,signer);
+    await contract.register(1);
+    alert("Successfully registered as teacher")
   }
   const userLogin= async () => {
-    console.log("clickede Login")
+     const provider=new BrowserProvider(window.ethereum);
+    const signer= await provider.getSigner();
+    const contract= new Contract(CONTRACT_ADDRESS,abi,signer);
+    const address=await signer.getAddress()
+    const role= await contract.roles[address];
+    alert("role=",role)
   }
   return (
     <>
