08c52b831f3c85537cc71b4101f35e3430690660
Tejuk0431 <kadaliteju4@gmail.com>
2026-02-13 05:57:13 -0500

    two dashboards created

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import abi from './contractabi.json'
 import { BrowserProvider, Contract } from 'ethers'
+import StudentDashboard from './studentdashboard'
+import TeacherDashboard from './teacherdashboard'
 function App() {
   const CONTRACT_ADDRESS="0x8Fc2B925ccfD2BC6Dc139577EF84E3995a58Fc93"
   const [count, setCount] = useState(0)
+  const [showstudent, setshowstudent]=useState(false)
+  const [showteacher, setshowteacher]=useState(false)
   const registerStudent= async () => {
     const provider=new BrowserProvider(window.ethereum);
     const signer= await provider.getSigner();
@@ -28,10 +32,10 @@ function App() {
     const address=await signer.getAddress()
     const role= await contract.getRole(address);
     if (role == 1){
-      alert ("you are a teacher") 
+      setshowteacher(true)
     }
     if (role == 2){
-      alert ("you are a student")
+      setshowstudent(true)
     }
   }
   return (
@@ -41,6 +45,8 @@ function App() {
       <button onClick={registerTeacher}>Register as Teacher</button>
       <br />
       <button onClick={registerStudent}>Register as Student</button>
+      {showstudent && < StudentDashboard></StudentDashboard>}
+      {showteacher && < TeacherDashboard></TeacherDashboard>}
     </>
   )
 }
---
+++ src/studentdashboard.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import abi from './contractabi.json'
+import { BrowserProvider, Contract } from 'ethers'
+function StudentDashboard() {
+  return (
+    <>
+      <h1>this is StudentDashboard</h1>
+    </>
+  )
+}
+
+export default StudentDashboard
+
